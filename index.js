70ac48414752d75916cfccb546dd929bf42fb052
krish203na <kanhapanchal03@gmail.com>
2024-07-08 14:33:36 +0530

    updated cors origin

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded())
 const server = http.createServer(app)
 const io = new Server(server,{
     cors:{
-        origin:"http://localhost:5173"
+        origin:"https://thunderous-bombolone-3f2d11.netlify.app/"
     }
 })
 
