6473a547bda41a68f6645a827bb972d4306d1bd7
krish203na <kanhapanchal03@gmail.com>
2024-07-10 23:55:59 +0530

    changed the approch

--- controllers/userController/user.controller.js
+++ controllers/userController/user.controller.js
@@ -4,26 +4,59 @@ const User = require("../../Models/user.model")
 
 const registerUser = asyncHandler(async (req, res) => {
 
-    const { userid, username, email, fullname } = req.body
+    // const { userid, username, email, fullname } = req.body
+
+    // try {
+    //     const existingUser = await User.findOne({ userid });
+
+    //     if (existingUser) {
+    //         return res.status(200).json({ message: 'User already exists' });
+    //     }
+
+    //     const user = new User({
+    //         userid,
+    //         username,
+    //         email,
+    //         fullname
+    //     });
+
+    //     await user.save();
+    //     res.status(201).json(user);
+    // } catch (error) {
+    //     res.status(400).json({ message: error.message });
+    // }
+
+
+    // ========================= second approch ============================
+
+    const { userId, email, fullName, username } = req.body;
 
     try {
-        const existingUser = await User.findOne({ userid });
+        let user = await User.findOne({ userid: userId });
 
-        if (existingUser) {
-            return res.status(200).json({ message: 'User already exists' });
+        if (!user) {
+
+            user = new User({
+                userid: userId,
+                username,
+                email,
+                fullname: fullName
+            });
+
+        } else {
+
+            user.email = email;
+            user.username = username;
+            user.fullname = fullName;
+
+
         }
+        user = await user.save(); // This line saves a new user document
 
-        const user = new User({
-            userid,
-            username,
-            email,
-            fullname
-        });
-
-        await user.save();
-        res.status(201).json(user);
+        res.status(200).json({ message: 'User data saved', user });
     } catch (error) {
-        res.status(400).json({ message: error.message });
+        console.error('Error saving/updating user data:', error);
+        res.status(500).json({ message: error.message });
     }
 })
 
